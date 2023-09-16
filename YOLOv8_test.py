3ca85d6f1d5d3472fb252ad6d212f8b1204363f9
joonryu69 <114625114+joonryu69@users.noreply.github.com>
2023-09-16 14:43:53 -0500

    Create YOLOv8_test.py

---
+++ YOLOv8_test.py
@@ -0,0 +1,58 @@
+from ultralytics import YOLO
+import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
+import cv2
+import supervision as sv
+
+
+
+def main():
+    
+    
+    # define resolution
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    # specify the model
+    model = YOLO("yolov8s.pt")
+
+    # customize the bounding box
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    while True:
+        ret, frame = cap.read()
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        labels = [
+        f"{model.names[confidence]} {class_id: 0.2f}"
+        for _, _, class_id,confidence, _
+        in detections
+        ]
+
+
+        frame = box_annotator.annotate(
+        scene=frame, 
+        detections=detections, 
+        labels=labels
+        ) 
+
+        
+
+        cv2.imshow("yolov8", frame)
+
+        if (cv2.waitKey(30) == 27): # break with escape key
+            break
+            
+    cap.release()
+    cv2.destroyAllWindows()
+    
+if __name__ == "__main__":
+    main()
+
